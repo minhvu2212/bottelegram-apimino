dd9d78a17e1edc0b49d045358fdeb9088dfc0d0e
minhvu2212 <tangminhvu2212@gmail.com>
2024-04-25 01:47:33 +0700

    222

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
+const fs = require('fs');
 const TOKEN = '7033297550:AAFlOvttWqse1k85lwX0IFkfKeXYm7llm9Q';
 const API_TOKEN = 'cS0VuiPlLH5mt69TEAAgyAWe2CmNrN9G';
 const HOST_URL = 'https://apptanglike.com/api/v1';
@@ -16,8 +17,22 @@ function createOrder(serverId, postLink, numberSeeding) {
   };
 
   return axios.post(url, payload, { headers: headers })
-    .then(response => response.data)
-    .catch(error => console.error(error));
+    .then(response => {
+      const orderData = response.data;
+      // Ghi dữ liệu vào file data.txt
+      fs.appendFile('data.txt', JSON.stringify(orderData) + '\n', (err) => {
+        if (err) {
+          console.error('Error writing to data.txt:', err);
+        } else {
+          console.log('Order data written to data.txt');
+        }
+      });
+      return orderData;
+    })
+    .catch(error => {
+      console.error('Error creating order:', error);
+      throw error;
+    });
 }
 
 bot.onText(/\/start/, (msg) => {
@@ -40,30 +55,32 @@ bot.onText(/\/start/, (msg) => {
 });
 
 bot.on('callback_query', (callbackQuery) => {
-    const message = callbackQuery.message;
-    const chatId = message.chat.id;
-    const serverId = callbackQuery.data;
-  
-    bot.sendMessage(chatId, `Bạn đã chọn server ${serverId}. Vui lòng gửi link cần tăng view.`);
-    bot.answerCallbackQuery(callbackQuery.id)
-      .then(() => {
-        bot.once('message', (msg) => {
-          if (msg.text && !msg.text.startsWith('/')) {
-            const postLink = msg.text;
-            bot.sendMessage(chatId, 'Vui lòng nhập số lượng seeding mong muốn:');
-            bot.once('message', (msg) => {
-              if (!isNaN(msg.text)) {
-                const numberSeeding = parseInt(msg.text);
-                createOrder(serverId, postLink, numberSeeding)
-                  .then(() => {
-                    bot.sendMessage(chatId, 'Đơn đặt hàng của bạn đã được tạo thành công!');
-                  });
-              } else {
-                bot.sendMessage(chatId, 'Số lượng seeding không hợp lệ. Vui lòng thử lại.');
-              }
-            });
-          }
-        });
+  const message = callbackQuery.message;
+  const chatId = message.chat.id;
+  const serverId = callbackQuery.data;
+
+  bot.sendMessage(chatId, `Bạn đã chọn server ${serverId}. Vui lòng gửi link cần tăng view.`);
+  bot.answerCallbackQuery(callbackQuery.id)
+    .then(() => {
+      bot.once('message', (msg) => {
+        if (msg.text && !msg.text.startsWith('/')) {
+          const postLink = msg.text;
+          bot.sendMessage(chatId, 'Vui lòng nhập số lượng seeding mong muốn:');
+          bot.once('message', (msg) => {
+            if (!isNaN(msg.text)) {
+              const numberSeeding = parseInt(msg.text);
+              createOrder(serverId, postLink, numberSeeding)
+                .then(() => {
+                  bot.sendMessage(chatId, 'Đơn đặt hàng của bạn đã được tạo thành công!');
+                })
+                .catch(error => {
+                  bot.sendMessage(chatId, 'Đã xảy ra lỗi khi tạo đơn đặt hàng. Vui lòng thử lại sau.');
+                });
+            } else {
+              bot.sendMessage(chatId, 'Số lượng seeding không hợp lệ. Vui lòng thử lại.');
+            }
+          });
+        }
       });
-  });
-  
+    });
+});
